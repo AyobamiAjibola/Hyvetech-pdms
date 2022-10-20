ddbd8fbfca24166245514d8a07443c04d511c721
jiffixTech <74901849+jiffixTech@users.noreply.github.com>
2022-10-20 09:48:49 +0100

    Sign up page modification

---
+++ server/src/models/TrackerWaitList.ts
@@ -0,0 +1,32 @@
+import {
+  AutoIncrement,
+  Column,
+  DataType,
+  Model,
+  PrimaryKey,
+  Table,
+} from "sequelize-typescript";
+
+import {
+  CreationOptional,
+  InferAttributes,
+  InferCreationAttributes,
+} from "sequelize";
+
+@Table({
+  timestamps: true,
+  paranoid: true,
+  tableName: "tracker_wait_lists",
+})
+export default class TrackerWaitList extends Model<
+  InferAttributes<TrackerWaitList>,
+  InferCreationAttributes<TrackerWaitList>
+> {
+  @PrimaryKey
+  @AutoIncrement
+  @Column({ type: DataType.INTEGER, field: "tracker_wait_list_id" })
+  declare id: CreationOptional<number>;
+
+  @Column(DataType.STRING)
+  declare email: string;
+}
