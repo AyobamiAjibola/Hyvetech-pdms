e8c5087ed250166e2c4743bdcef3369222291e01
uchepercynnoch <uchennoch@gmail.com>
2022-12-28 18:14:02 +0100

    Refactoring

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,4 @@
-import https, { ServerOptions } from 'https';
 import http from 'http';
-import fs from 'fs';
-import path from 'path';
 
 import 'dotenv/config';
 
@@ -9,43 +6,20 @@ import app, { corsOptions } from './app';
 import startup from './startup';
 import AppLogger from './utils/AppLogger';
 import { Server as SocketServer } from 'socket.io';
-import settings from './config/settings';
-
 
 const logger = AppLogger.init('server').logger;
 const port = process.env.PORT || 5050;
 
+const server = http.createServer(app);
 
-if (settings.service.env === 'production') {
-  const serverOptions: ServerOptions = {
-    key: fs.readFileSync(path.resolve(process.env.SSL_KEY_FILE as string)),
-    cert: fs.readFileSync(path.resolve(process.env.SSL_CRT_FILE as string)),
-  };
+const io = new SocketServer(server, {
+  cors: corsOptions,
+});
 
-  const server = https.createServer(serverOptions, app);
-  server.listen(port, () => logger.info(`Server running on port: ${port}`));
-  const io = new SocketServer(server, {
-    cors: corsOptions,
-  });
+io.on('connection', socket => {
+  logger.info(socket.id);
+});
 
-  io.on('connection', socket => {
-    logger.info(socket.id);
-  });
+startup(io).catch(console.error);
 
-  startup(io).catch(console.error);
-} else {
-  const server = http.createServer(app);
-  server.listen(port, () => logger.info(`Server running on port: ${port}`));
-  const io = new SocketServer(server, {
-    cors: corsOptions,
-  });
-
-  io.on('connection', socket => {
-    logger.info(socket.id);
-  });
-
-  startup(io).catch(console.error);
-}
-
-
-
+server.listen(port, () => logger.info(`Server running on port: ${port}`));
