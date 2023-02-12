9fa119aeb92b2e37283dea43d7a0998879d6ad2d
Seyi_Coded <opadonuseyi01@gmail.com>
2023-02-12 18:38:31 +0100

    ..

--- server/build/startup/index.js
+++ server/build/startup/index.js
@@ -7,7 +7,6 @@ const rabbitmq_email_manager_1 = require("rabbitmq-email-manager");
 const agenda_schedule_wrapper_1 = require("agenda-schedule-wrapper");
 const database_1 = __importDefault(require("../config/database"));
 const dataStore_1 = __importDefault(require("../config/dataStore"));
-const CommandLineRunner_1 = __importDefault(require("../helpers/CommandLineRunner"));
 const constants_1 = require("../config/constants");
 const queue_1 = __importDefault(require("../config/queue"));
 const agendaManager_1 = __importDefault(require("../services/agendaManager"));
@@ -17,9 +16,9 @@ async function startup(io) {
     dataStore_1.default.init();
     await database_1.default.init();
     const mongodb = await database_1.default.mongodb();
-    await database_1.default.sequelize.sync({ alter: true });
+    // await database.sequelize.sync({ alter: true });
     // console.log('reac0')
-    await CommandLineRunner_1.default.run();
+    // await CommandLineRunner.run();
     // console.log('reac')
     await rabbitmq_email_manager_1.QueueManager.init({
         queueClient: queue_1.default.client,
