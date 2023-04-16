7a783f5a6f220e90bf0bfcdcb917fd5193844cec
ayobami ajibola <ayobamiajibola46@gmail.com>
2023-04-16 17:59:56 +0100

    invoice

--- server/src/controllers/InvoiceController.ts
+++ server/src/controllers/InvoiceController.ts
@@ -454,7 +454,7 @@ export default class InvoiceController {
 
       return invoice;
     });
-    console.log(invoices)
+
     return Promise.resolve({
       code: HttpStatus.OK.code,
       message: HttpStatus.OK.value,
@@ -1075,7 +1075,7 @@ export default class InvoiceController {
     if (!invoice) return Promise.reject(CustomAPIError.response(`Invoice not found`, HttpStatus.NOT_FOUND.code));
 
     const count = estimate?.count && estimate?.count - 1
-    await estimate?.update({ status: ESTIMATE_STATUS.draft, count: count });
+    await estimate?.update({ status: ESTIMATE_STATUS.sent, count: count });
     await Invoice.destroy({ where: { id: +invoiceId }, force: true });
 
     return Promise.resolve({
