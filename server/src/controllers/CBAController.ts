cda880b6c45342f04d9108a57bb205e078d93626
charles <charles.onuorah@yahoo.com>
2023-05-24 20:07:08 +0100

    fix

--- server/src/controllers/CBAController.ts
+++ server/src/controllers/CBAController.ts
@@ -163,6 +163,12 @@ class CBAController {
 
     if (partnerAccount) return partnerAccount;
 
+    if (partner.email.trim() === '')
+      return Promise.reject(CustomAPIError.response('Please provide partner email', HttpStatus.BAD_REQUEST.code));
+
+    if (partner.phone.trim() === '')
+      return Promise.reject(CustomAPIError.response('Please provide partner phone', HttpStatus.BAD_REQUEST.code));
+
     const response = dataSources.partnerAccountDaoService.create({
       firstName: accountRequest.businessName,
       lastName: 'Ltd',
