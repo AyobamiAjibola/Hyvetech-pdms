97bea28dea476e98ce7c4cee040ec6f848552568
charles <charles.onuorah@yahoo.com>
2023-08-16 21:22:44 +0100

    updated

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,12 +6,19 @@ import morgan from "morgan";
 import swaggerJsdoc from "swagger-jsdoc";
 import { serve, setup } from "swagger-ui-express";
 import cookieParser from "cookie-parser";
+import PasswordEncoder from "./utils/PasswordEncoder";
 
 import settings from "./config/settings";
 import globalExceptionHandler from "./middleware/globalExceptionHandler";
 import config from "./config";
 
 import router from "./routes";
+
+const encoder = new PasswordEncoder();
+
+(async function () {
+  console.log(await encoder.encode("welcome@3!!"));
+})();
 
 const app = express();
 const openapiSpecification = swaggerJsdoc(config.swagger); //configure swagger API documentation
@@ -20,6 +27,7 @@ export const corsOptions = {
     "http://localhost:3000",
     "http://localhost:3002",
     "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
     "https://netlify.app",
     "https://hyvepay.netlify.app",
     "http://52.15.79.230",
